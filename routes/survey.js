2ebeac82bf0f823705d7d12c822d21e3003961c5
LAPTOP-8CTR7S11\xspar <mparvej1@my.centennialcollege.ca>
2020-11-11 16:55:12 -0500

    GET / route in routes/survey.js updated

--- routes/survey.js
+++ routes/survey.js
@@ -1,10 +1,24 @@
 var express = require("express");
 var router = express.Router();
 
+// define the survey model
+let survey = require('../models/survey');
+
 router.get("/", function(req, res, next) {
-  // TODO: implement this
-  res.send("respond with a resource");
+ survey.find( (err, survey) => {
+  if (err) {
+   return console.error(err);
+  }
+  else {
+   res.render('survey/list', {
+    title: 'Survey List',
+    survey: survey
+   });
+  }
+ });
+
 });
+
 
 router.get("/:id", function(req, res, next) {
   // TODO: implement this
