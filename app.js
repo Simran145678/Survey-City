7029e4a3b4046c5987d2ba8e9462ab7c3211bec7
Devesh Kumar <deveshkumar2107@hotmail.com>
2020-11-12 01:57:59 -0500

    first commit

--- app.js
+++ app.js
@@ -22,57 +22,58 @@ const mainRouter = require("./routes/_main");
 
 // return a factory function
 module.exports = async mongooseUri => {
-  await database.connect(mongooseUri);
+    await database.connect(mongooseUri);
 
-  // http app setup
-  const app = express();
+    // http app setup
+    const app = express();
 
-  // passport setup
-  passport.use(User.createStrategy());
-  passport.serializeUser(User.serializeUser());
-  passport.deserializeUser(User.deserializeUser());
+    // passport setup
+    passport.use(User.createStrategy());
+    passport.serializeUser(User.serializeUser());
+    passport.deserializeUser(User.deserializeUser());
 
-  // view engine setup
-  app.set("views", path.join(__dirname, "views"));
-  app.set("view engine", "ejs");
+    // view engine setup
+    app.set("views", path.join(__dirname, "views"));
+    app.set("view engine", "ejs");
 
-  // other middleware setup
-  app.use(logger("dev"));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
-  app.use(expressSession({
-    secret: process.env.SESSION_SECRET || "It's a plane in the sky!",
-    saveUninitialized: false,
-    resave: true,
-    store: new MongoStore({
-      mongooseConnection: database.connection,
-      ttl: 7 * 24 * 60 * 60,
-      touchAfter: 24 * 60 * 60,
-    }),
-  }));
-  app.use(flash());
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(express.static(path.join(__dirname, "public")));
+    // other middleware setup
+    app.use(logger("dev"));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(expressSession({
+        secret: process.env.SESSION_SECRET || "It's a plane in the sky!",
+        saveUninitialized: false,
+        resave: true,
+        store: new MongoStore({
+            mongooseConnection: database.connection,
+            ttl: 7 * 24 * 60 * 60,
+            touchAfter: 24 * 60 * 60,
+        }),
+    }));
+    app.use(flash());
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(express.static(path.join(__dirname, "public")));
+    app.use(express.static(path.join(__dirname, 'node_modules')));
 
-  app.use("/", mainRouter);
+    app.use("/", mainRouter);
 
-  // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    next(createError(404));
-  });
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+        next(createError(404));
+    });
 
-  // error handler
-  app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+    // error handler
+    app.use(function(err, req, res, next) {
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error", { title: "Something went wrong! • SurveyCity" });
-  });
+        // render the error page
+        res.status(err.status || 500);
+        res.render("error", { title: "Something went wrong! • SurveyCity" });
+    });
 
-  return app;
+    return app;
 };
--- routes/survey.js
+++ routes/survey.js
@@ -1,24 +1,100 @@
 var express = require("express");
+const survey = require("../models/survey");
 var router = express.Router();
+let Survey = require('../models/survey')
 
 router.get("/", function(req, res, next) {
-  // TODO: implement this
-  res.send("respond with a resource");
+    // TODO: implement this
+    output = Survey.find((err, surveyList) => {
+        if (err) {
+            output = err;
+        } else {
+            output = surveyList;
+            res.render('survey/surveyList', { title: 'Survey', SurveyList: surveyList });
+        }
+    });
 });
 
-router.get("/:id", function(req, res, next) {
-  // TODO: implement this
-  res.send("respond with a resource");
+router.get("/add", function(req, res, next) {
+    res.render('survey/addSurvey', {
+        title: 'Add Survey',
+    })
 });
 
-router.post("/:id", function(req, res, next) {
-  // TODO: implement this
-  res.send("respond with a resource");
+router.post("/add", function(req, res, next) {
+    let newSurvey = Survey({
+        //"owner": req.body.owner,
+        "title": req.body.title,
+        "description": req.body.description,
+        //"questions": req.body.questions,
+    })
+    Survey.create(newSurvey, (err, Survey) => {
+        if (err) {
+            console.log(err);
+            res.end(err);
+        } else {
+            // refresh the book list
+            res.redirect('/survey');
+        }
+    })
 });
 
-router.get("/:id/delete", function(req, res, next) {
-  // TODO: implement this
-  res.send("respond with a resource");
+
+router.get("/edit/:id", function(req, res, next) {
+    let id = req.params.id;
+
+    Survey.findById(id, (err, surveyToEdit) => {
+
+        if (err) {
+            console.log(err);
+            res.end(err);
+        } else {
+            //show the edit view 
+            res.render('survey/editSurvey', {
+                title: 'Edit Survey',
+                Survey: surveyToEdit,
+
+            });
+        }
+    });
+});
+
+router.post("/edit/:id", function(req, res, next) {
+    let id = req.params.id;
+
+    let updateSurvey = Survey({
+        "_id": id,
+        //"owner": req.body.owner,
+        "title": req.body.title,
+        "description": req.body.description,
+        //"questions": req.body.questions,
+    });
+
+    Survey.updateOne({ _id: id }, updateSurvey, (err) => {
+        if (err) {
+            console.log(err);
+            res.end(err);
+        } else {
+            // refresh the Survey
+            res.redirect('/survey');
+        }
+    });
+});
+
+router.get("/delete/:id", function(req, res, next) {
+    // TODO: implement this
+    let id = req.params.id;
+
+    Survey.remove({ _id: id }, (err) => {
+
+        if (err) {
+            console.log(err);
+            res.end(err);
+        } else {
+            // refresh the survey list
+            res.redirect('/survey');
+        }
+    });
 });
 
 module.exports = router;
