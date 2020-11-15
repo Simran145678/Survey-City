5c629d3aaca06651304f2a43b66b1ffaf7403136
Aeriit <aeriit@hobbelisk.com>
2020-11-15 15:12:07 -0500

    Fix code style issues in survey route

--- routes/survey.js
+++ routes/survey.js
@@ -1,52 +1,40 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-// define the survey model
-let survey = require('../models/survey');
+const survey = require("../models/survey");
 
-router.get("/", function(req, res, next) {
- survey.find( (err, survey) => {
-  if (err) {
-   return console.error(err);
-  }
-  else {
-   res.render('survey/list', {
-    title: 'Survey List',
-    survey: survey
-   });
-  }
- });
+router.get("/", function (req, res) {
+  survey.find((err, survey) => {
+    if (err)
+      throw err;
 
+    res.render("survey/list", {
+      title: "Survey List",
+      surveys: survey
+    });
+  });
 });
 
 
-router.get("/:id", function(req, res, next) {
+router.get("/:id", function (req, res) {
   // TODO: implement this
   res.send("respond with a resource");
 });
 
-router.post("/:id", function(req, res, next) {
+router.post("/:id", function (req, res) {
   // TODO: implement this
   res.send("respond with a resource");
 });
 
-router.get("/:id/delete", function(req, res, next) {
-  // TODO: implement this
-  res.send("respond with a resource");
-  let id = req.params.id;
+router.get("/:id/delete", function (req, res) {
+  const id = req.params.id;
 
- survey.remove({_id: id}, (err) => {
-    if(err)
-    {
-        console.log(err);
-        res.end(err);
-    }
-    else
-    {
-        //refresh the Survey list
-        res.redirect('/survey');
-    }
- })
+  survey.remove({ _id: id }, err => {
+    if (err)
+      throw err;
+
+    res.redirect("/survey");
+  });
 });
 
 module.exports = router;
